32d635c7ebd7e6c29ee32e76ac976a4c99b3eb9a
Vladimir Zemskov <vlzemskovs@gmail.com>
2019-05-17 15:21:14 +0300

    add post postmessage

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
-window.app = {
-	notification: {
-		alert: () => {
-			window.parent.postMessage("data", "*");
+if (window.navigator.userAgent === "Mozilla/5.0 Cordova") {
+	window.app = {
+		notification: {
+			alert: () => {
+				window.parent.postMessage({ notification: "alert" }, "*");
+			}
 		}
-	}
-};
+	};
+} else {
+	window.app = {
+		notification: {
+			alert: () => {
+				alert("It Browser")
+			}
+		}
+	};
+}
 
 let showMessage = window.app.notification.alert;
 let btn = document.getElementById("btn-alert");
